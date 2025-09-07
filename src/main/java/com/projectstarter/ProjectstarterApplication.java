639fba2c51d1061bd2a6d8fccabf21b81a6cec7b
ibrahimguenichi <ibrahimguenichi@gmail.com>
2025-09-07 15:07:02 +0100

    feat: intialize springboot project

---
+++ src/main/java/com/projectstarter/ProjectstarterApplication.java
@@ -0,0 +1,13 @@
+package com.projectstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectstarterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectstarterApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/projectstarter/ProjectstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectstarterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
