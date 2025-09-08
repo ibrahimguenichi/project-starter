aed63a3e28dbaecae3689b6081c2616dbffbe21a
ibrahimguenichi <ibrahimguenichi@gmail.com>
2025-09-08 23:20:55 +0100

    feat: prepare infra

---
+++ src/main/java/auth/domain/AbstartcEntity.java
@@ -0,0 +1,17 @@
+package auth.domain;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+public class AbstartcEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+}
